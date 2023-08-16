b921217a280f41b942861bb5111cb4b5c152538a
Laertis Loutsis <lloutsis@triplelift.com>
2023-08-16 14:59:13 +0200

    Add debug verbose logging in http server request

--- relay/http_middlewares.go
+++ relay/http_middlewares.go
@@ -3,6 +3,10 @@ package relay
 import (
 	"compress/gzip"
 	"net/http"
+	"net/http/httputil"
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
 	"time"
 )
 
@@ -18,8 +22,40 @@ func allMiddlewares(h *HTTP, handlerFunc relayHandlerFunc) relayHandlerFunc {
 func (h *HTTP) logMiddleWare(next relayHandlerFunc) relayHandlerFunc {
 	return relayHandlerFunc(func(h *HTTP, w http.ResponseWriter, r *http.Request, start time.Time) {
 		if h.log {
-			h.logger.Println("got request on: " + r.URL.Path)
+			h.logger.Println("got request from: " + r.RemoteAddr + " on: " + r.URL.Path)
+
+			// DEBUG
+			h.logger.Printf("\nDEBUG\nr.URL.Host   : %s \nr.URL.Path   : %s \nr.URL.RawPath: %s \nr.Host       : %s \nr.RemoteAddr : %s \nr.RequestURI : %s \n", r.URL.Host, r.URL.Path, r.URL.RawPath, r.Host, r.RemoteAddr, r.RequestURI)
+			reqDump, err := httputil.DumpRequest(r, true)
+			if err != nil {
+				h.logger.Fatal(err)
+			}
+			h.logger.Printf("REQUEST:\n%s", string(reqDump))
 		}
+
+		var bodyBytes []byte
+		var err error
+
+		if r.Body != nil {
+			bodyBytes, err = ioutil.ReadAll(r.Body)
+			if err != nil {
+				h.logger.Printf("Body reading error: %v", err)
+			}
+			defer r.Body.Close()
+		}
+
+		h.logger.Printf("Headers: %+v\n", r.Header)
+
+		if len(bodyBytes) > 0 {
+			var prettyJSON bytes.Buffer
+			if err = json.Indent(&prettyJSON, bodyBytes, "", "\t"); err != nil {
+				h.logger.Printf("JSON parse error: %v", err)
+			}
+			h.logger.Println(string(prettyJSON.Bytes()))
+		} else {
+			h.logger.Printf("Body: No Body Supplied\n")
+		}
+
 		next(h, w, r, start)
 	})
 }

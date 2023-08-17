5ff29eb0cac2b432a990d2f50149f6ce31e3dffb
Laertis Loutsis <lloutsis@triplelift.com>
2023-08-17 15:48:21 +0200

    Shorten and simplify DEBUG logging on http server requests

--- relay/http_middlewares.go
+++ relay/http_middlewares.go
@@ -4,9 +4,6 @@ import (
 	"compress/gzip"
 	"net/http"
 	"net/http/httputil"
-	"bytes"
-	"encoding/json"
-	"io/ioutil"
 	"time"
 )
 
@@ -25,35 +22,11 @@ func (h *HTTP) logMiddleWare(next relayHandlerFunc) relayHandlerFunc {
 			h.logger.Println("got request from: " + r.RemoteAddr + " on: " + r.URL.Path)
 
 			// DEBUG
-			h.logger.Printf("\nDEBUG\nr.URL.Host   : %s \nr.URL.Path   : %s \nr.URL.RawPath: %s \nr.Host       : %s \nr.RemoteAddr : %s \nr.RequestURI : %s \n", r.URL.Host, r.URL.Path, r.URL.RawPath, r.Host, r.RemoteAddr, r.RequestURI)
 			reqDump, err := httputil.DumpRequest(r, true)
 			if err != nil {
 				h.logger.Fatal(err)
 			}
-			h.logger.Printf("REQUEST:\n%s", string(reqDump))
-		}
-
-		var bodyBytes []byte
-		var err error
-
-		if r.Body != nil {
-			bodyBytes, err = ioutil.ReadAll(r.Body)
-			if err != nil {
-				h.logger.Printf("Body reading error: %v", err)
-			}
-			defer r.Body.Close()
-		}
-
-		h.logger.Printf("Headers: %+v\n", r.Header)
-
-		if len(bodyBytes) > 0 {
-			var prettyJSON bytes.Buffer
-			if err = json.Indent(&prettyJSON, bodyBytes, "", "\t"); err != nil {
-				h.logger.Printf("JSON parse error: %v", err)
-			}
-			h.logger.Println(string(prettyJSON.Bytes()))
-		} else {
-			h.logger.Printf("Body: No Body Supplied\n")
+			h.logger.Printf("\nREQUEST START:\n%s\nREQUEST END\n", string(reqDump))
 		}
 
 		next(h, w, r, start)
